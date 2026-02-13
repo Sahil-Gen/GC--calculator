478943351c66989c0671d19d58766ccffd1647fe
sahil <sahil912p@gmail.com>
2026-02-13 23:35:17 +0530

    Update gc_calculator.py

--- gc_calculator.py
+++ gc_calculator.py
@@ -1,5 +1,5 @@
 def gc_content(sequence):
-    sequence = sequence.strip().upper()      # <-- important fix
+    sequence = sequence.strip().upper()
 
     g = sequence.count("G")
     c = sequence.count("C")
@@ -7,11 +7,55 @@ def gc_content(sequence):
     t = sequence.count("T")
 
     valid = g + c + a + t
+
     if valid == 0:
         return 0.0
 
     return round(((g + c) / valid) * 100, 2)
 
+
+def read_fasta(filename):
+    sequences = {}
+    header = None
+    seq = []
+
+    with open(filename, "r") as file:
+        for line in file:
+            line = line.strip()
+
+            # Skip empty lines
+            if not line:
+                continue
+
+            # Header line
+            if line.startswith(">"):
+                if header:
+                    sequences[header] = "".join(seq)
+
+                header = line[1:]   # Remove ">"
+                seq = []
+
+            else:
+                seq.append(line)
+
+        # Save last sequence
+        if header:
+            sequences[header] = "".join(seq)
+
+    return sequences
+
+
 if __name__ == "__main__":
-    seq = input("Enter DNA sequence: ").strip()
-    print("GC Content:", gc_content(seq), "%")
+
+    filename = input("Enter FASTA file name: ")
+
+    try:
+        fasta_sequences = read_fasta(filename)
+
+        for header, sequence in fasta_sequences.items():
+            gc = gc_content(sequence)
+            print(f"{header}")
+            print(f"GC Content: {gc}%\n")
+
+    except FileNotFoundError:
+        print("Error: File not found. Check file name and path.")
